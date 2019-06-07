8b0a65f01a792f88162ffc527ac29fb55df295a0
Josh Braun <braunjoshuaw@gmail.com>
2019-06-07 11:32:32 -0700

    Remove preview icon from app

--- src/components/accordion_card.jsx
+++ src/components/accordion_card.jsx
@@ -195,7 +195,6 @@ class AccordionCard extends Component {
             <td>{challenge.fields['Reward Occurrence']}</td>
             <td onDoubleClick={(e) => this.editPoints(e, challenge)}>{challenge.fields['Points']} ({challenge.fields['Total Points']})</td>
             <td className="actions text-center">
-              <img className="table-icon preview-icon" src={hasBeenEdited ? 'images/icon_preview_notification.svg' : 'images/icon_preview.svg'} onClick={() => this.props.setPreviewChallenge(challenge)} />
               <img className="table-icon delete-icon" src="images/icon_delete.svg" onClick={() => this.openDeleteConfirmModal(challenge)} />
             </td>
           </tr>
