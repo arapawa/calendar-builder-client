33445222ab2666383c36f2894c16da177848a058
Danny Peck <dannytpeck@gmail.com>
2019-06-13 15:02:51 -0700

    Update accordion card to be a functional component

--- src/components/accordion_card.jsx
+++ src/components/accordion_card.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import Airtable from 'airtable';
